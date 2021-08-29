0dc58c0dbce64ab5584f275c6cdbf2860953cf88
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2021-08-30 01:04:38 +0200

    Cleanup

--- src/elm/UI/Glsl/Generator.elm
+++ src/elm/UI/Glsl/Generator.elm
@@ -1,4 +1,4 @@
-module UI.Glsl.Generator exposing (Constant, Context, ErrorValue(..), Expression, Expression1, Expression2, Expression3, Expression33, Expression4, ExpressionX, File, FunDecl, GlslValue(..), Mat3, Statement, TypedName, TypingFunction, Vec2, Vec3, Vec4, abs2, abs4, abs_, add, add2, add4, ands, arr, assign, assignAdd, assignBy, atan2_, by, by2, by3, byF, ceil_, constant, cos_, cosh, cross, decl, def, def2, def3, def4, div, div2, divConst, divF, dot, dotted1, dotted2, dotted3, dotted33, dotted4, eq, exp, expressionToGlsl, false, fileToGlsl, float, floatCast, floatT, floatToGlsl, for, forLeq, fract, fun0, fun1, fun2, fun3, fun4, funDeclToGlsl, fwidth, geq, gl_FragColor, gl_FragCoord, gt, hl2rgb, if_, int, intCast, intT, interpret, length, leq, log, log2, lt, mat3T, mat3_3_3_3, max3, max4, max_, min_, minusOne, mix, mod, negate2, negate_, neq, normalize, normalize3, one, ors, pow, radians_, return, round_, sign, sin_, sinh, smoothstep, statementToGlsl, subtract, subtract2, subtract3, subtract4, tan_, ternary, ternary3, true, uNsAfEtYpEcAsT, uniform, unknown, unknownFunDecl, unknownStatement, unsafeBreak, unsafeCall, unsafeContinue, unsafeNop, value, valueToString, vec2, vec2T, vec2Zero, vec3, vec3T, vec3Zero, vec4, vec4T, vec4Zero, vec4_1_3, vec4_3_1, voidT, zero)
+module UI.Glsl.Generator exposing (Constant, Context, ErrorValue(..), Expression, Expression1, Expression2, Expression3, Expression33, Expression4, ExpressionX, File, FunDecl, GlslValue(..), Mat3, Statement, TypedName, TypingFunction, Vec2, Vec3, Vec4, abs2, abs4, abs_, add, add2, add3, add4, adds2, adds3, adds4, ands, arr, assign, assignAdd, assignBy, atan2_, by, by2, by3, byF, ceil_, constant, cos_, cosh, cross, decl, def, def2, def3, def4, div, div2, divConst, divF, dot, dotted1, dotted2, dotted3, dotted33, dotted4, eq, exp, expressionToGlsl, false, fileToGlsl, float, floatCast, floatT, floatToGlsl, for, forLeq, fract, fun0, fun1, fun2, fun3, fun4, funDeclToGlsl, fwidth, geq, gl_FragColor, gl_FragCoord, gt, hl2rgb, if_, int, intCast, intT, interpret, length, leq, log, log2, lt, mat3T, mat3_3_3_3, max3, max4, max_, min_, minusOne, mix, mod, negate2, negate_, neq, normalize, normalize3, one, ors, pow, radians_, return, round_, sign, sin_, sinh, smoothstep, statementToGlsl, subtract, subtract2, subtract3, subtract4, tan_, ternary, ternary3, true, uNsAfEtYpEcAsT, uniform, unknown, unknownFunDecl, unknownStatement, unsafeBreak, unsafeCall, unsafeContinue, unsafeNop, value, valueToString, vec2, vec2T, vec2Zero, vec3, vec3T, vec3Zero, vec4, vec4T, vec4Zero, vec4_1_3, vec4_3_1, voidT, zero)
 
 import Dict exposing (Dict)
 import Expression exposing (RelationOperation(..))
@@ -458,6 +458,36 @@ ors es =
             List.foldl (\e a -> expr2 Or a e) h t
 
 
+adds2 : List Expression2 -> Expression2
+adds2 es =
+    case es of
+        [] ->
+            vec2Zero
+
+        h :: t ->
+            List.foldl (\e a -> add2 a e) h t
+
+
+adds3 : List Expression3 -> Expression3
+adds3 es =
+    case es of
+        [] ->
+            vec3Zero
+
+        h :: t ->
+            List.foldl (\e a -> add3 a e) h t
+
+
+adds4 : List Expression4 -> Expression4
+adds4 es =
+    case es of
+        [] ->
+            vec4Zero
+
+        h :: t ->
+            List.foldl (\e a -> add4 a e) h t
+
+
 true : Expression1 Bool
 true =
     dotted1 <| Expression <| Bool True
@@ -526,6 +556,11 @@ add =
 add2 : ExpressionX a Vec2 -> ExpressionX b Vec2 -> Expression2
 add2 =
     expr22 Add
+
+
+add3 : ExpressionX a Vec3 -> ExpressionX b Vec3 -> Expression3
+add3 =
+    expr23 Add
 
 
 add4 : ExpressionX a Vec4 -> ExpressionX b Vec4 -> Expression4
