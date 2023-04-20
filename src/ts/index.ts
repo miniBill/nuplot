b2e1305ff306ee87d478dd34c80efdd855123980
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-04-21 01:34:21 +0200

    Updating service worker setup

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,3 +2,9 @@ import { Elm } from "../optimized.js";
 import { init } from "./main";
 
 init(Elm);
+
+if ("serviceWorker" in navigator)
+  navigator.serviceWorker.register(
+    new URL("../service-worker.ts", import.meta.url),
+    { type: "module" }
+  );
