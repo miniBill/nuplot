45b1ee3b21d3703d3b69e280ba11966cae61f7b3
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-02-20 13:45:05 +0100

    Simplify compilation

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,98 @@
-import { Elm } from "../optimized.js";
-import { init } from "./main";
+import { ElmType } from "../elm/UI.elm";
+import { NuPlot } from "./nuplot";
+import { KaTeXElement } from "./katex";
+import localForage from "localforage";
 
-init(Elm);
+declare class ClipboardItem {
+    constructor(data: { [mimeType: string]: Blob });
+}
+
+customElements.define("nu-plot", NuPlot);
+customElements.define("ka-tex", KaTeXElement);
+
+const storageKey = "documents";
+
+function fromLS() {
+    const saved: { [key: string]: string } = {};
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        if (key === null) continue;
+        const value = localStorage.getItem(key);
+        if (value === null) continue;
+        saved[key] = value;
+    }
+    return saved;
+}
+
+export async function init(Elm: ElmType) {
+    let saved;
+    try {
+        saved = await (localForage.getItem(storageKey) as Promise<{
+            [key: string]: string;
+        }>);
+    } catch {
+        saved = fromLS();
+    }
+
+    const node = document.getElementById("main");
+    if (node == null) {
+        document.write(
+            "Error initializing application. This might be caused by a browser extension."
+        );
+        return;
+    }
+
+    const app = Elm.UI.init({
+        node: node,
+        flags: {
+            saved: saved,
+            hasClipboard: typeof ClipboardItem !== "undefined",
+            hasFullscreen: "onfullscreenchange" in node,
+            languages: "languages" in navigator ? navigator.languages : [],
+            rootUrl: window.location.toString(),
+            googleAccessToken: localStorage.getItem("googleAccessToken") ?? "",
+        },
+    });
+    window.addEventListener("storage", (e) => {
+        if (e.key == "googleAccessToken")
+            app.ports.gotGoogleAccessToken.send(e.newValue ?? "");
+    });
+    app.ports.persist.subscribe((value) => {
+        localForage.setItem(storageKey, value);
+    });
+    app.ports.save.subscribe((id) => {
+        const element = document.getElementById(id) as NuPlot;
+        element?.save();
+    });
+    app.ports.fullscreen.subscribe((id) => {
+        const element = document.getElementById(id)?.parentElement;
+        element?.requestFullscreen();
+    });
+    app.ports.exitFullscreen.subscribe((id) => document.exitFullscreen());
+    if ("onfullscreenchange" in document)
+        document.addEventListener("fullscreenchange", (e) =>
+            app.ports.isFullscreen.send(document.fullscreenElement !== null)
+        );
+    app.ports.resetZoom.subscribe((id) => {
+        const element = document.getElementById(id) as NuPlot;
+        element?.resetZoom(true);
+    });
+    app.ports.copy.subscribe((id) => {
+        const element = document.getElementById(id) as NuPlot;
+        element?.copy();
+    });
+    app.ports.saveGoogleAccessToken.subscribe((token) => {
+        localStorage.setItem("googleAccessToken", token);
+    });
+    app.ports.saveGoogleAccessTokenAndCloseWindow.subscribe((token) => {
+        localStorage.setItem("googleAccessToken", token);
+        window.close();
+    });
+    app.ports.openWindow.subscribe((url) => {
+        window.open(
+            url,
+            "popup",
+            "menubar=no,status=no,toolbar=no,width=400,height=400"
+        );
+    });
+}
